78db922d887c88703e564adff2bf298470f89b7e
Franck Aragão <fjunior.aragao@gmail.com>
2020-12-23 22:58:59 -0300

    fix(plugin): hook access head

--- example/nuxt.config.js
+++ example/nuxt.config.js
@@ -1,19 +1,19 @@
-const { resolve } = require('path')
+const { resolve } = require("path");
 
 module.exports = {
-  rootDir: resolve(__dirname, '..'),
-  buildDir: resolve(__dirname, '.nuxt'),
+  rootDir: resolve(__dirname, ".."),
+  buildDir: resolve(__dirname, ".nuxt"),
   head: {
-    title: 'nuxt-maps-module'
+    title: "nuxt-maps-module",
   },
   srcDir: __dirname,
   render: {
-    resourceHints: false
+    resourceHints: false,
   },
-  modules: [{ handler: require('../') }],
+  modules: [{ handler: require("../") }],
   publicRuntimeConfig: {
     gmaps: {
-      key: 'AIzaSyB52Qw4eFfBtB9L6B4AXf7OZxsW0CVf3M0'
-    }
-  }
-}
+      key: "",
+    },
+  },
+};
